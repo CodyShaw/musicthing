38a5183389bf65dbe49d2f89291f2a09b0c54f1a
Cody Shaw <cody.de.shaw@gmail.com>
2020-08-31 23:47:09 -0700

    GUI Works but threads need work...

--- musicthing/ViewController.swift
+++ musicthing/ViewController.swift
@@ -9,17 +9,54 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
+    //Array of Flats
     let notesFlats = ["C", "D♭", "D", "E♭", "E", "F", "G♭", "G", "A♭", "A", "B♭", "B"]
 
+    //Array of Sharps
     let notesSharps = ["C", "C♯", "D", "D♯", "E", "F", "F♯", "G", "G♯", "A", "B♯", "B"]
     
-    var note = 0
+    //Control Globals
+    var runningRandomNote = false
+    var randomNoteTime = 0.0
+    
+    //Text Field Outlet
+    @IBOutlet weak var NoteText: NSTextField!
+    //Randomize Note Outlet
+    @IBOutlet weak var randomizeNoteButton: NSButton!
+    
+    //Randomize the displayed note, to be ran in a thread
+    func randomizeNote(){
+        //Run until cancelled
+        while(true){
+            //Make Thread to update text field on GUI
+            DispatchQueue.main.asyncAfter(deadline: .now() + randomNoteTime){
+                self.changeNote(note: self.notesFlats[Int.random(in: 0..<12)])
+            }
+        }
+    }
+    
+    //Changes the Note in the GUI
+    func changeNote(note: String){
+        NoteText.stringValue = note
+    }
+    
+    //Empty DispatchWorkItem to turn on and off when button is pressed
+    var displayRandomNote = DispatchWorkItem(){}
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        //Load the workitem
+        displayRandomNote = DispatchWorkItem(){
+            self.randomizeNote()
+        }
+        
+        //Display a note by default
+        DispatchQueue.main.async {
+            self.changeNote(note: self.notesFlats[0])
+        }
     }
 
     override var representedObject: Any? {
@@ -28,48 +65,36 @@ class ViewController: NSViewController {
         }
     }
     
-    @IBOutlet weak var NoteText: NSTextField!
     
+    //Button Click function
     @IBAction func changeNoteClick(_ sender: NSButton) {
+        //Flip the current setting
+        runningRandomNote = !runningRandomNote
         
-        note += 1
-        
-        if(note >= 12){
-            note = 0;
-        }
-        
-        NoteText.stringValue = notesFlats[note]
-        
-        
-        
-        let queue = DispatchQueue(label: "testQueue")
-
-        queue.async {
+        if(runningRandomNote){
             
-            var i = 0;
-            
-            while(true){
-                sleep(10)
-                
-                self.changeNote(note: self.notesFlats[i])
-                
-                i = i + 1
-                
-                if(i >= 12){
-                    i = 0
-                }
+            DispatchQueue.main.async {
+                self.randomizeNoteButton.stringValue = "Generating..."
             }
+            
+            DispatchQueue.global().async(execute: displayRandomNote)
+            
+        } else {
+            
+            DispatchQueue.main.async {
+                self.randomizeNoteButton.stringValue = "Start Notes"
+            }
+            
+            DispatchQueue.main.async {
+                self.displayRandomNote.cancel()
+            }
+            
         }
-        
+
     }
-    
-    func changeNote(note: String){
-        
-        NoteText.stringValue = note
-        
-    }
-    
-    
+
 }
+
+
 
 
